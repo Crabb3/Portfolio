3b4b983beae6e41f899049e18fb0032bc39d8685
Denny Hsu <denny890413@gmail.com>
2024-05-29 00:35:53 +0800

    update some side project

--- src/app/components/SPcard.js
+++ src/app/components/SPcard.js
@@ -6,25 +6,72 @@ import Link from "next/link";
 const SPcard = (props) => {
   const prop = props;
   return (
-    <div className="relative w-72 h-96">
+    <div className="relative min-w-60 w-72 h-96">
       <div className="card border-2 border-[#848] rounded-md">
         <div className="frontCard">
-          <Image fill src={prop.ImageSrc} alt={prop.ImageSrc} />
+          <Image
+            fill
+            src={prop.ImageSrc}
+            alt={prop.ImageSrc}
+            objectFit="cover"
+          />
           <h2 className="text-2xl px-2 bottom-0 right-0 absolute bg-gray-400/[0.7] text-black">
             {prop.Title}
           </h2>
         </div>
-        <div className="backCard p-4 flex flex-col">
-          <p>{prop.content}</p>
-          <p>{prop.content}</p>
-          <p>{prop.content}</p>
-          <p>{prop.content}</p>
-          <p>{prop.content}</p>
-          <p>{prop.content}</p>
-          <div className="flex justify-evenly justify-self-end">
-            {prop.gh && <button>GitHub</button>}
-            {prop.src && <Link href={prop.src}>LiveDemo</Link>}
-            {prop.yt && <button>YT</button>}
+        <div className="backCard p-4 flex flex-col justify-between">
+          <div>
+            <h2 className="my-4 text-2xl font-bold flex justify-center">
+              {prop.Title}
+            </h2>
+            <p>{prop.content}</p>
+          </div>
+          <div className="relative flex-col flex bottom-0 left-0">
+            <div className="flex gap-4 justify-evenly">
+              {prop.gh && (
+                <Link
+                  href={prop.gh}
+                  className="border-2 border-[#848] rounded-full px-2 py-1 flex justify-center items-center"
+                >
+                  <Image
+                    className="inline-block"
+                    src="/icon/Github.svg"
+                    alt={prop.gh}
+                    width={20}
+                    height={20}
+                  />
+                </Link>
+              )}
+              {prop.src && (
+                <Link
+                  href={prop.src}
+                  className="border-2 border-[#848] rounded-full px-2 py-1 flex justify-center items-center"
+                >
+                  <Image
+                    className="inline-block"
+                    src="/icon/reactjs.svg"
+                    alt={prop.src}
+                    width={20}
+                    height={20}
+                  />
+                </Link>
+              )}
+              {prop.yt && (
+                <Link
+                  href={prop.yt}
+                  className="border-2 border-[#848] rounded-full px-2 py-1 flex justify-center items-center"
+                >
+                  <Image
+                    className="inline-block"
+                    src="/icon/youtube.svg"
+                    alt={prop.yt}
+                    width={20}
+                    height={20}
+                  />
+                </Link>
+              )}
+            </div>
+            <div className="text-md text-gray-500 mt-2">{prop.tag}</div>
           </div>
         </div>
       </div>
--- src/app/sp/page.js
+++ src/app/sp/page.js
@@ -10,27 +10,43 @@ const page = () => {
       <div className="flex gap-4 mt-4 justify-evenly flex-wrap">
         <SPcard
           src="https://tmhsu-0413.github.io/"
-          Title="Link-in-bio website"
+          Title="Link-in-bio"
           gh="https://github.com/TMHsu-0413/TMHsu-0413.github.io"
           content="一頁式網站，搭配一些美化特效"
           ImageSrc="/spImage/Link_in_bio.jpg"
-          tag="#react #vanta.js"
+          tag="#react.js #vanta.js #tailwindCSS"
         />
         <SPcard
-          src="https://tmhsu-0413.github.io/"
-          Title="Link in bio"
-          gh="https://github.com/TMHsu-0413/TMHsu-0413.github.io"
-          content="一頁式網站，搭配一些美化特效"
-          ImageSrc="/spImage/Link_in_bio.jpg"
-          tag="#react #vanta.js"
+          src="https://graph-visualizer-three.vercel.app/graph"
+          Title="Graph Visualizer"
+          gh="https://github.com/TMHsu-0413/Graph-Visualizer"
+          content="可根據LeetCode題目為Graph或Tree選擇，並動態產生出拓樸，以便debug時使用"
+          ImageSrc="/spImage/Graph_visual.jpg"
+          tag="#next.js #d3.js #tailwindCSS"
         />
         <SPcard
-          src="https://tmhsu-0413.github.io/"
-          Title="Link in bio"
-          gh="https://github.com/TMHsu-0413/TMHsu-0413.github.io"
-          content="一頁式網站，搭配一些美化特效"
-          ImageSrc="/spImage/Link_in_bio.jpg"
-          tag="#react #vanta.js"
+          Title="Anonymous Chatroom"
+          gh="https://github.com/TMHsu-0413/CCU_web_hw5"
+          yt="https://youtu.be/897qX4mINIk"
+          content="免登入匿名聊天室，提供一對一與公開區域，可以傳送系統預設貼圖或圖片，並有語音及視訊功能"
+          ImageSrc="/spImage/Chatroom.jpg"
+          tag="#react.js #express.js #socket.io"
+        />
+        <SPcard
+          Title="Weather Forecast"
+          gh="https://github.com/TMHsu-0413/CCU_web_hw4"
+          yt="https://youtu.be/zMlGZl0fOxc"
+          content="天氣查詢系統，根據使用者查詢位置回傳當地天氣狀況，可以選擇查詢後是否寄到信箱"
+          ImageSrc="/spImage/Weather.jpg"
+          tag="#react.js #PHP #axios #MySQL"
+        />
+        <SPcard
+          Title="Blog Life"
+          gh="https://github.com/TMHsu-0413/CCU_web_hw3"
+          yt="https://youtu.be/BmyGGveNl6w"
+          content="模擬部落格功能"
+          ImageSrc="/spImage/Blog.jpg"
+          tag="#react.js #PHP #Bootstrap #MySQL"
         />
       </div>
     </div>
